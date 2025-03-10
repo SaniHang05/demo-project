942e2bace3160ee5c3df6277215c4237f18ef842
CodingBoy16 <rs1644818@gmail.com>
2025-03-10 23:32:55 +0530

    Check

--- frontUI/src/components/AboutSRM.jsx
+++ frontUI/src/components/AboutSRM.jsx
@@ -4,7 +4,7 @@ const AboutSRM = ({img,title,desc}) => {
   return (
     <div className='opacity: 1; transform: none; m-4 shadow-2xl'>
     <div className="md:w-96 h-[26rem] mx-auto rounded-lg text-card-foreground shadow-sm p-4 bg-gray-800/50 backdrop-blur-lg border-gray-500 hover:bg-gray-900/70 transition-all duration-300 dropShadow">
-      <img src={img} className='rounded-full w-40 mx-auto m-2'/>
+      <img src={img} className='rounded-full w-40 mx-auto m-2' loading='lazy'/>
       <h4 className='text-white pt-2'>{title}</h4>
       <p className='text-gray-400 pb-3'>{desc}</p>
     </div>
--- frontUI/src/components/Course.jsx
+++ frontUI/src/components/Course.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import * as motion from "motion/react-client"
 
 const Course = ({icon,header,para}) => {
   return (
-    <div className='opacity: 1; transform: none; m-4 shadow-2xl'>
+    <motion.div  
+    initial={{ opacity: 0, scale: 0 }} 
+    animate={{animation:'ease-in',animationDuration:3000,animationDelay:4000,animationTimeline:'inherit', opacity:1,scale:1}}
+    className='opacity: 1; transform: none; m-4 shadow-2xl'>
     <div className="md:w-96 h-52 mx-auto rounded-lg text-card-foreground shadow-sm p-6 bg-gray-800/50 backdrop-blur-lg border-gray-800 hover:bg-gray-900/70 transition-all duration-300 text-center">
     <i class={`${icon} text-4xl text-teal-200`}></i>
       <p className="text-xl font-medium text-white my-3">{header}</p>
       <p className="text-gray-400 text-lg font-light">{para}</p>
     </div>
-    </div>
+    </motion.div>
 
   )
 }
